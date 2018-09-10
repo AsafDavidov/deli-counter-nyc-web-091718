0668432ea525b16089858bc1dbae0892337d80cf
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 18:02:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line(katz_deli)
   if katz_deli.size < 1
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     return "The line is currently:katz_deli.size"
   end
