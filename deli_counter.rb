56b0c345a955ad7d907d5f2f69dd13b742b57a4d
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 18:01:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,10 @@ def take_a_number(katz_deli,person)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.size < 1
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
