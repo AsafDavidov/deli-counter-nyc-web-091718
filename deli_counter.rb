920bf84481e52d6ef8f1b8d99e9814a40d08772a
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 17:58:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
   else
     return "The line is currently:katz_deli.size"
 end
+
 def take_a_number(katz_deli,person)
   katz_deli.push(person)
   puts "Welcome, #{person}. You are number #{katz_deli.size} in line"
